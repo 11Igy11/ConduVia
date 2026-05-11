dc7c72601bbcf1be3223a11deb6a451480049d93
igi_t <iimpric@gmail.com>
2026-05-11 15:04:08 +0200

    Align PCAP service chart bars

--- ui/pcap_page.py
+++ ui/pcap_page.py
@@ -389,8 +389,8 @@ class PcapPage(QWidget):
             "Visible service groups",
             value_key="count",
             value_label_key="value",
-            label_width=190,
-            label_limit=38,
+            label_width=175,
+            label_limit=34,
             max_rows=24,
         )
         self.chart_activity = BarChartWidget(
